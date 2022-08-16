b532229b746aa8f213c1b40a509700980092f10a
Barry Alexander <barry.alexander@gmail.com>
2022-08-16 14:44:39 -0700

    simplify mapping

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -3,28 +3,18 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "hardhat/console.sol";
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+contract YourContract {
 
-contract YourContract is Ownable {
-
-    mapping(address => uint256) public accounts;
+    mapping(address => uint256) public balance;
 
     constructor() payable {
     }
 
     function deposit (uint256 amount) public {
-      accounts[msg.sender] = accounts[msg.sender] + amount;
-      console.log("deposit >> account balance: ", accounts[msg.sender]);
+      balance[msg.sender] = balance[msg.sender] + amount;
     }
 
     function checkBalance () public view returns (uint256) {
-      console.log("checkBalance >> account balance: ", accounts[msg.sender]);
-      return accounts[msg.sender];
+      return balance[msg.sender];
     }
-
-    // to support receiving ETH by default
-    receive() external payable {}
-
-    fallback() external payable {}
 }
